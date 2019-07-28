7cb0fdfc08822e417fdbae47dfd8adabbf0e37d5
Matiur <matiurrahman1993@gmail.com>
2019-07-29 00:27:32 +0600

    Add one method squre in branchB

--- hello.java
+++ hello.java
@@ -2,8 +2,12 @@ public class Hello{
     public static void main(String args[]){
         System.out.println("Hello world");
         gretting("Matiur Rahman");
+        system.out.println(int squre());
     }
     public static void gretting(String name){
         System.out.println("My name is "+ name);
+    }
+    public static int squre(int x){
+        return x*x;
     }
 }
